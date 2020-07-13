261a41bfc9c467019d454a9bec00eba541b45e46
Timothy-Flavin <binaryOwlizard@gmail.com>
2020-07-13 10:30:10 -0500

    Added more options that I thought I had uploaded already oof

--- SwolBotDataCollector.py
+++ SwolBotDataCollector.py
@@ -9,6 +9,7 @@ while videosLeftToDowload=='y':
     print(yt.streams.filter(adaptive=True))
     print("enter tag you want to input ex: 137, or the letter 'y' if you simply want the highest resolution")
     itag = input()
+    print("downloading...")
     if(itag!='y' and itag!='Y'):
         yt.streams.get_by_itag(itag).download('./YoutubeData')
     else:
@@ -25,7 +26,11 @@ while videosLeftToDowload=='y':
         print("save video clip as ex (good_pushup_1): ", end='')
         fileName=input()
         video.write_videofile("./YoutubeData/"+fileName+".mp4",fps=30)
+        print("what are the labels for this clip? separated by commas\nexample: knees apart,back bent")
+        clipLabels=input()
+        file2 = open(r"./YoutubeData/"+fileName+".txt","w")
+        file2.write(clipLabels)
         print("would you like to pick another clip from this video? 'y' or 'n': ", end='')
         pickingClips = input()
-    print("would you like to pick another clip? 'y' or 'n': ", end='')
+    print("would you like to pick another youtube video? 'y' or 'n': ", end='')
     videosLeftToDowload=input()
