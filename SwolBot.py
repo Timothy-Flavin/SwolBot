2db127cd122c86cca38b93239447a8a6c572f00a
Timothy Flavin <BinaryOwlizard@gmail.com>
2020-07-26 18:02:07 -0500

    might work

--- SwolBot.py
+++ SwolBot.py
@@ -10,7 +10,27 @@ input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 # Test model on random input data.
-input_shape = input_details[0]['shape']
+height = input_details[0]['shape'][1]
+width = input_details[0]['shape'][2]
+
+float_model = input_details[0]['dtype'] == np.float32
+if float_model:
+    template_input = (np.float32(template_input) - 127.5) / 127.5
+    target_input = (np.float32(target_input) - 127.5) / 127.5
+else:
+  print("Not a float model")
+
+# Set the value of the input tensor
+interpreter.set_tensor(input_details[0]['index'], template_input)
+# Run the calculations
+interpreter.invoke()
+# Extract output data from the interpreter
+template_output_data = interpreter.get_tensor(output_details[0]['index'])
+template_offset_data = interpreter.get_tensor(output_details[1]['index'])
+
+
+
+
 input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
 interpreter.set_tensor(input_details[0]['index'], input_data)
 
